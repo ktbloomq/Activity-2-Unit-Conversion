59b92cca80e479cc00211e42404aacbb6a3b5b37
Kevin <ktb@bloomquist.com>
2022-10-02 14:12:54 -0700

    Add project files.

---
+++ Activity 2 - Unit Conversion/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_2___Unit_Conversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            float farenheit = 0;
+            try {
+                farenheit = float.Parse(Input.Text) * 9 / 5 + 32;
+                Output.Text = farenheit.ToString();
+            } catch {
+                Output.Text = "error";
+            }
+        }
+    }
+}
